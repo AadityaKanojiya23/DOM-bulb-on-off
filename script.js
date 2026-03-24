d6cc2434194ccc961e786ea72e9d1740d21ac7a4
AdityaKanojiya23 <adityastudent750@gmail.com>
2026-03-25 00:41:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let bulb =  document.querySelector('#bulb')
+let btn = document.querySelector('button')
+
+let flag = 0 
+
+btn.addEventListener('click' , ()=>{
+    if(flag == 0){
+         bulb.style.backgroundColor = "yellow"
+         console.log('clicked');
+         flag = 1
+         
+    }else {
+         bulb.style.backgroundColor = "transparent"
+         console.log('reclick');
+         flag = 0    
+    }
+})
